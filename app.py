4ab70d4ceaab86d4fb3bf8be2081786c2505c77e
AlejandroTC <aletamayo-02@hotmail.com>
2023-10-08 20:57:49 -0600

    finish, no gcp

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 #MYSQL CONNECTION
-app.config['MYSQL_HOST'] = '104.198.72.148'
+app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'aleadk'
+app.config['MYSQL_PASSWORD'] = 'admin'
 app.config['MYSQL_DB'] = 'store'
 mysql = MySQL(app)
 
@@ -15,12 +15,12 @@ app.secret_key = 'mysecretkey'
 
 @app.route('/')
 def Index():
-    # cur = mysql.connection.cursor()
-    # cur.execute('SELECT * FROM employees')
-    # data = cur.fetchall()
-    # print(data)
-    # return render_template('index.html', employees = data)
-    return render_template('index.html')
+    cur = mysql.connection.cursor()
+    cur.execute('SELECT * FROM employees')
+    data = cur.fetchall()
+    print(data)
+    return render_template('index.html', employees = data)
+
 #CRUD
 @app.route('/add', methods=['POST'])
 def add_contact():
@@ -28,10 +28,12 @@ def add_contact():
         name = request.form['name']
         lname = request.form['last_name']
         bdate = request.form['birth_date']
-        # cur = mysql.connection.cursor()
-        # cur.execute('INSERT INTO employees (nombre, apellido, fecha_nacimiento) VALUES (%s, %s, %s)'),
-        # (name, lname, bdate)
-        # mysql.connection.commit()
+        print(name)
+        print(lname)
+        print(bdate)
+        cur = mysql.connection.cursor()
+        cur.execute('INSERT INTO employees (nombre, apellido, fecha_nacimiento) VALUES (%s, %s, %s)', (name, lname, bdate))
+        mysql.connection.commit()
         flash('Employee added successfully')
         return redirect(url_for('Index'))
 
@@ -40,8 +42,7 @@ def get_employee(id):
     cur = mysql.connection.cursor()
     cur.execute('SELECT * FROM employees WHERE id = %s', (id) )
     data = cur.fetchall()
-    print(data)
-    return redirect(url_for('edit.html', employee = data[0]))
+    return render_template('edit.html', employee = data[0])
 
 @app.route('/update/<id>', methods = ['POST'])
 def update_employee(id):
@@ -50,21 +51,16 @@ def update_employee(id):
         lname = request.form['last_name']
         bdate = request.form['birth_date']
         cur = mysql.connection.cursor()
-        cur.execute("""
-            UPDATE employees
-            SET nombre = %s
-                apellido = %s
-                fecha_nacimiento = %s
-            WHERE id = %s
-        """, (nombre, lname, bdate, id))
+        cur.execute("UPDATE employees SET nombre = %s, apellido = %s, fecha_nacimiento = %s WHERE id = %s", (nombre, lname, bdate, id))
         mysql.connection.commit()
         flash('Employee has been edited')
         return redirect(url_for('Index'))
 
-@app.route('/delete/<string:id>')
+@app.route('/delete/<id>')
 def delete_employ(id):
     cur = mysql.connection.cursor()
-    cur.execute('DELETE FFROM employees WHERE id = {0}'.format(id))
+    cur.execute('DELETE FROM employees WHERE id = {0}'.format(id))
+    print('DELETE FFROM employees WHERE id= {0}'.format(id))
     mysql.connection.commit()
     flash('Employee removed successfully')
     return redirect(url_for('Index'))
